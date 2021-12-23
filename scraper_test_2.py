efcbbbb1aa9097f13ce0c22b0a875af7ffeaaa5e
Christopher Hipp Kuut <79537682+christopher-hipp@users.noreply.github.com>
2021-12-23 19:05:09 +0200

    Delete unnecessary code. Add function to print out two randomly selected words.

--- scraper_test_2.py
+++ scraper_test_2.py
@@ -1,36 +1,18 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = "https://www.vocabulary.com/dictionary/randomword"
 
-htmlfile = urllib.request.urlopen(url)
-soup = BeautifulSoup(htmlfile, 'lxml')
+def word_getter():
+	for i in range(2):
+		url = "https://www.vocabulary.com/dictionary/randomword"
 
-word = soup.find_all("h1")
+		htmlfile = urllib.request.urlopen(url)
+		soup = BeautifulSoup(htmlfile, 'lxml')
 
-soup1 = soup.find(class_="short")
+		temp = soup.find("div", class_="word-area")
+		word = temp.find("h1")
+		print(word.get_text())
 
-try:
-    soup1 = soup1.get_text()
-except AttributeError:
-    print('Cannot find such word! Check spelling.')
-    exit()
 
-# Print short meaning
-print ('-' * 25 + '->', word, "<-" + "-" * 25)
-print ("SHORT MEANING: \n\n", soup1)
-print ('-' * 65)
-
-# Print long meaning
-soup2 = soup.find(class_="long")
-soup2 = soup2.get_text()
-print ("LONG MEANING: \n\n",soup2)
-
-print ('-' * 65)
-
-# Print instances like Synonyms, Antonyms, etc.
-soup3 = soup.find(class_="instances") 
-txt = soup3.get_text()
-txt1 = txt.rstrip()
-
-print (' '.join(txt1.split()))
+if __name__ == "__main__":
+	word_getter()
