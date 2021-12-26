fa2b72fd0207c337c406e23fb8e7a604d2240c13
Christopher Hipp Kuut <79537682+christopher-hipp@users.noreply.github.com>
2021-12-26 12:56:47 +0200

    Remove whitespace from output.

--- scraper_test_2.py
+++ scraper_test_2.py
@@ -2,17 +2,13 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
-def word_getter():
+if __name__ == "__main__":
 	for i in range(2):
 		url = "https://www.vocabulary.com/dictionary/randomword"
 
 		htmlfile = urllib.request.urlopen(url)
 		soup = BeautifulSoup(htmlfile, 'lxml')
-
-		temp = soup.find("div", class_="word-area")
-		word = temp.find("h1")
-		print(word.get_text())
-
-
-if __name__ == "__main__":
-	word_getter()
+		area_finder_in_html = soup.find("div", class_="word-area")
+		specific_word = area_finder_in_html.find("h1")
+		word_into_string_maker = specific_word.get_text().split()
+		print(word_into_string_maker)
