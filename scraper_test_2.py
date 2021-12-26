fd801020b56a5630c736b9823682ed8f95ef7b53
Christopher Hipp Kuut <79537682+christopher-hipp@users.noreply.github.com>
2021-12-26 18:55:48 +0200

    Did something? But not much. Reformat.

--- scraper_test_2.py
+++ scraper_test_2.py
@@ -1,14 +1,23 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-if __name__ == "__main__":
-	for i in range(2):
-		url = "https://www.vocabulary.com/dictionary/randomword"
 
+if __name__ == "__main__":
+	counter = int(input("Enter number: "))
+	
+	# Do for given number of times
+	for n in range(counter):
+		# Save url as string and parse webpage with BeautifulSoup
+		url = "https://www.vocabulary.com/dictionary/randomword"
 		htmlfile = urllib.request.urlopen(url)
 		soup = BeautifulSoup(htmlfile, 'lxml')
+		
+		# Find specific word
 		area_finder_in_html = soup.find("div", class_="word-area")
 		specific_word = area_finder_in_html.find("h1")
+		
+		# Conver word into string
 		word_into_string_maker = specific_word.get_text().split()
 		word_as_string = ' '.join([str(elem) for elem in word_into_string_maker])
-		print(f"{word_as_string}")
+		
+		print(word_as_string)
